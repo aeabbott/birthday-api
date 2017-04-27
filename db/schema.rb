589dee235602237c117d98fb460d0c732c576041
aeabbott <amandaeabbott@gmail.com>
2017-04-27 14:25:12 -0400

    Commit schema file based lasted db changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170425230208) do
+ActiveRecord::Schema.define(version: 20170426235057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20170425230208) do
     t.date     "born_on"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_birthdays_on_user_id", using: :btree
   end
 
   create_table "examples", force: :cascade do |t|
@@ -42,5 +44,6 @@ ActiveRecord::Schema.define(version: 20170425230208) do
     t.index ["token"], name: "index_users_on_token", unique: true, using: :btree
   end
 
+  add_foreign_key "birthdays", "users"
   add_foreign_key "examples", "users"
 end
